2bb5786be5004434b021eb587591be02ba028adb
Shelestenko Nikolay <37602817@qip.ru>
2016-11-10 09:57:02 +0300

    job file handler

---
+++ www/pdf.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+</head>
+<body>
+<form action="http://kr-pro.ru/object/sendpdf/" method='post'>
+
+	lot:<input name="id" type="text"><br>
+	email:<input name="email" type="text"><br>
+	<input type="submit">
+</form>
+
+
+
