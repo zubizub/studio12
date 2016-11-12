54cc587060480ef5098bfbae5afb83bdf1ce31cf
Rinaz Shakirov <rinaz.shakir@gmail.com>
2016-11-12 14:15:32 +0300

    fix

--- www/local/templates/studio12/header.php
+++ www/local/templates/studio12/header.php
@@ -9,7 +9,7 @@ IncludeTemplateLangFile(__FILE__);
 <head>
        
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title><?$APPLICATION->ShowTitle()?></title>
     <meta name="description" content="Studio21">
