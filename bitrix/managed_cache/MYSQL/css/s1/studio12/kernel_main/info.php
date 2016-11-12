4e87b051c5fddf13521eb857b4a68691fe4fe532
Rinaz Shakirov <rinaz.shakir@gmail.com>
2016-11-12 19:49:58 +0300

    fix

--- bitrix/managed_cache/MYSQL/css/s1/studio12/kernel_main/info.php
+++
@@ -1,1 +0,0 @@
-<? $arFilesInfo = array( 'FILES' => array("/bitrix/js/main/core/css/core.css" => "14789685242854","/bitrix/js/main/core/css/core_popup.css" => "147896852429699","/bitrix/js/main/core/css/core_date.css" => "14789685249657",), 'CUR_SEL_CNT' => '414', 'CUR_IE_CNT' => '0'); ?>
--- bitrix/managed_cache/MYSQL/user_option/f0/f0fc06318e8ac66f82152783eff5c4d8.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001478968694';
-$dateexpire = '001478972294';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:1:{s:8:"settings";a:3:{s:4:"edit";s:3:"off";s:9:"collapsed";s:2:"on";s:3:"fix";s:2:"on";}}}';
-return true;
-?>
