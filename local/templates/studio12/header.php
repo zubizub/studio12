2a7fbfe5458e1e1c958e0cb23f868c5511fcaea6
zubizub <zubizubwork@gmail.com>
2016-11-14 11:11:30 +0300

    google tags

--- local/templates/studio12/header.php
+++ local/templates/studio12/header.php
@@ -22,7 +22,21 @@ IncludeTemplateLangFile(__FILE__);
     <script>
         (function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement)
     </script>
-    
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TDW2RL');</script>
+<!-- End Google Tag Manager -->
+
+Также вставьте непосредственно после открывающего тега <body> следующий фрагмент:
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TDW2RL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript)-->
+
 </head>
 
 
