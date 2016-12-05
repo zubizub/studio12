7f9a1d27ebeadea9f72469a3aaafd078f9394c73
Rinaz Shakirov <rinaz.shakir@gmail.com>
2016-12-05 22:17:43 +0300

    fix SVG animate IE

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ $APPLICATION->SetTitle("Studio#12");
         <div class="wrapper">
             <div class="section__inner">
                 <div class="box">
-                    <svg class="dynamic" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewbox="0 0 1165 1500" height="1500px" width="1165px" enable-background="new 0 0 1165 1500" xml:space="preserve" preserveAspectRatio="xMinYMin meet" style="position: absolute; top: 0; left: 0; bottom: 0; right: 0;">
+                    <svg class="dynamic" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewbox="0 0 1165 1000" height="1000px" width="1165px" enable-background="new 0 0 1000 500" xml:space="preserve" preserveAspectRatio="xMinYMin meet" style="position: absolute; top: 0; left: 0; bottom: 0; right: 0;">
                         <path class="path" id="head"></path>
                     </svg>
                     <svg class="logo__front" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="1165px" height="509px" viewbox="0 0 1165 509" enable-background="new 0 0 1165 509" xml:space="preserve">
