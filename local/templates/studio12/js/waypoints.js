0a193ddbeb513afc2f834d5b6ae68a8640cd44d8
Rinaz Shakirov <rinaz.shakir@gmail.com>
2016-12-19 15:50:50 +0300

    fix

--- local/templates/studio12/js/waypoints.js
+++ local/templates/studio12/js/waypoints.js
@@ -1,34 +1,32 @@
 $(function () {
-    (function () {
-        $('[class*=anim--js-]').each(function() {
-            var self = this;
-            $(this).waypoint(function(direction) {
-                $(this).addClass('anim--run');
-                setTimeout(function () {
-                    $('.anim-dom', self).addClass('anim-dom--run');
-                }, 500);
-            }.bind(this), {
-                offset: '100%'
-            });
+    $('[class*=anim--js-]').each(function() {
+        var self = this;
+        $(this).waypoint(function(direction) {
+            $(this).addClass('anim--run');
+            setTimeout(function () {
+                $('.anim-dom', self).addClass('anim-dom--run');
+            }, 500);
+        }.bind(this), {
+            offset: '100%'
         });
-        $('.count-section').each(function() {
-            var self = this;
-            $(this).waypoint(function(direction) {
-                $('.count-anim', self).each(function () {
-                    var dataAnim = $(this).attr('data-number');
-                    $(this).prop('Counter', 0).animate({
-                        Counter: dataAnim
-                    }, {
-                        duration: 2000,
-                        easing: 'swing',
-                        step: function (now) {
-                            $(this).text(Math.ceil(now));
-                        }
-                    });
+    });
+    $('.count-section').each(function() {
+        var self = this;
+        $(this).waypoint(function(direction) {
+            $('.count-anim', self).each(function () {
+                var dataAnim = $(this).attr('data-number');
+                $(this).prop('Counter', 0).animate({
+                    Counter: dataAnim
+                }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
                 });
-            }.bind(this), {
-                offset: '100%'
             });
+        }.bind(this), {
+            offset: '100%'
         });
-    }());
+    });
 });
