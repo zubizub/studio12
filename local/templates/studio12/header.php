406de5a5a68cffd4474854ac3648bc883c2d96aa
Rinaz Shakirov <rinaz.shakir@gmail.com>
2016-12-19 16:24:35 +0300

    fix

--- local/templates/studio12/header.php
+++ local/templates/studio12/header.php
@@ -10,6 +10,7 @@ IncludeTemplateLangFile(__FILE__);
 
        
     <meta charset="utf-8">
+    <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title><?$APPLICATION->ShowTitle()?></title>
